9244c2d7d86de50325b8f993567050a740015b87
Farkhatzhan Makhmudov <faralost@gmail.com>
2023-01-13 22:56:45 +0600

    OAuth2PasswordRequestForm

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from blog import schemas, models
@@ -12,13 +13,13 @@ router = APIRouter(
 
 
 @router.post('/login')
-def login(user: schemas.Login, db: Session = Depends(get_db)):
-    a_user = db.query(models.User).filter(models.User.email == user.email).first()
+def login(user: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    a_user = db.query(models.User).filter(models.User.email == user.username).first()
     if not a_user or not verify_password(a_user.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(data={"sub": user.email})
+    access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
